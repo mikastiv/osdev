b1b38cb36b1aa58ddf6ffdb78b59b1b89ac1ab85
mikastiv <mikastiv@outlook.com>
2025-09-17 18:45:48 -0300

    initial

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "zig flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
+    zls.url = "github:zigtools/zls";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, zig, zls, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zig.packages.${system}.master
+            zls.packages.${system}.zls
+            curl
+            qemu
+            lldb
+          ];
+        };
+      });
+}
+
